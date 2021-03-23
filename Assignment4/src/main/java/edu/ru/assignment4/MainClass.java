c500da4042dd5d556572216f7e497da226440f55
gruiz123 <78010168+gruiz123@users.noreply.github.com>
2021-03-23 14:48:26 -0500

    new

---
+++ Assignment4/src/main/java/edu/ru/assignment4/MainClass.java
@@ -0,0 +1,39 @@
+package edu.ru.assignment4;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class MainClass {
+
+	public static void main(String[] args) throws Exception {
+
+		String inputFile = "names.txt";
+		String outputFile = "sorted_names.txt";
+
+		FileReader fileReader = new FileReader(inputFile);
+		BufferedReader bufferedReader = new BufferedReader(fileReader);
+		String inputLine;
+		List<String> lineList = new ArrayList<String>();
+		while ((inputLine = bufferedReader.readLine()) != null) {
+			lineList.add(inputLine);
+		}
+		fileReader.close();
+
+		Collections.sort(lineList);
+
+		FileWriter fileWriter = new FileWriter(outputFile);
+		PrintWriter out = new PrintWriter(fileWriter);
+		for (String outputLine : lineList) {
+			out.println(outputLine);
+		}
+		out.flush();
+		out.close();
+		fileWriter.close();
+
+	}
+}
---
+++ Assignment4ExtraCredit/src/main/java/edu/ru/assignment4extracredit/MainClass.java
@@ -0,0 +1,31 @@
+package edu.ru.assignment4extracredit;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+public class MainClass {
+
+    public static void main(String[] args) throws IOException {
+        // Test
+        System.out.println(getRandomWord("C:\\Users\\Jerry\\CST250Assignments\\Assignment4ExtraCredit\\names.txt"));
+    }
+
+    public static String getRandomWord(String path) throws IOException {
+        List<String> words = new ArrayList<String>();
+        try (BufferedReader reader = new BufferedReader(new FileReader(path))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                String[] wordline = line.split("\\s+");
+                for (String word : wordline) {
+                    words.add(word);
+                }
+            }
+        }
+        Random rand = new Random();
+        return words.get(rand.nextInt(words.size()));
+    }
+}
